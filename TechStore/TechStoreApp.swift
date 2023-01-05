9d38fefa804a61c4284339f5a3a6ec232b8d649f
Ryu0118 <87907656+Ryu0118@users.noreply.github.com>
2023-01-05 18:39:23 +0900

    first commit

---
+++ TechStore/TechStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechStoreApp.swift
+//  TechStore
+//
+//  Created by Ryu on 2023/01/05.
+//
+
+import SwiftUI
+
+@main
+struct TechStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
