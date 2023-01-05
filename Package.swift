aaf0175544408125a55fd80119fc80ffd9e7813c
Ryu0118 <87907656+Ryu0118@users.noreply.github.com>
2023-01-05 18:56:28 +0900

    マルチモジュールに対応

---
+++ Package.swift
@@ -0,0 +1,53 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TechStore",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        .library(name: "Domain", targets: ["Domain"]),
+        .library(name: "Presentation", targets: ["Presentation"]),
+        .library(name: "Infrastructure", targets: ["Infrastructure"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "0.47.2")
+    ],
+    targets: [
+        .target(
+            name: "Domain",
+            dependencies: [
+                .product(name: "Dependencies", package: "swift-composable-architecture"),
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
+            ]
+        ),
+        .target(
+            name: "Presentation",
+            dependencies: [
+                "Domain",
+                .product(name: "Dependencies", package: "swift-composable-architecture"),
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
+            ]
+        ),
+        .target(
+            name: "Infrastructure",
+            dependencies: [
+                "Domain",
+                .product(name: "Dependencies", package: "swift-composable-architecture"),
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
+            ]
+        ),
+        .testTarget(
+            name: "TechStoreTests",
+            dependencies: [
+                "Domain",
+                "Presentation",
+                .product(name: "Dependencies", package: "swift-composable-architecture"),
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
+            ]
+        ),
+    ]
+)
