9428447a9be97d9aebff6741a5112202859b595c
Ryu0118 <ryunosuke.shibuya.3d@stu.hosei.ac.jp>
2023-01-08 18:10:34 +0900

    add: APIRequestを作成

---
+++ Sources/Domain/APIRequest.swift
@@ -0,0 +1,37 @@
+// 
+//  APIRequest.swift
+//  
+//
+//  Created by ryunosuke.shibuya on 2023/01/08.
+//
+
+import Foundation
+
+public enum HTTPMethod: String {
+    case get = "GET"
+}
+
+public protocol GithubAPIRequest {
+    var apiUrl: String { get }
+    var path: String { get }
+    var method: HTTPMethod { get }
+    var queryItems: [URLQueryItem] { get }
+}
+
+public extension GithubAPIRequest {
+    func buildRequest() -> URLRequest? {
+        var urlComponents = URLComponents(string: apiUrl)
+        urlComponents?.queryItems = queryItems
+        
+        guard var url = urlComponents?.url else {
+            return nil
+        }
+        
+        url = url.appendingPathComponent(path)
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        
+        return request
+    }
+}
